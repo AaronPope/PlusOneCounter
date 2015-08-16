1b37ace2bf950bbcd5176084eb84649104f96038
tzemati <tzemati@gmail.com>
2015-08-16 10:46:00 -0400

    updated the way the player name displays on focus and lose focus

--- ui_controller.js
+++ ui_controller.js
@@ -4,20 +4,18 @@ var decrementButton = document.getElementById("decrement");
 var progressText = document.getElementById("progress");
 var testPlayer = new Player("Jerry", 0);
 var testGame = new Game();
+var numberOfPlayers = 1;
 
 //Set initial score to 0
 //scoreText.innerHTML = 0;
 $(document).ready(function() {
     scoreText.text(0);
-    $("#player-name").css("width", "50%");
+    //$("#player-name").css("width", "50%");
     $("#menu-toggle").hide();
     $("#number-players-selector").hide();
     $("#close-menu").hide();
     
-    $("#quiz").append("<br /><button id='add-player-button'>Add</button>");
-    $("#quiz").append("<h1>Score Tracker 2</h1>"
-                        + "<input placeholder='Player 2' />"
-                        + "<h2 id='p2-score'></h2>");
+    $("#players").append("<br /><button id='add-player-button'>Add</button>");
 });
 
 $("#add-player-button").on("click", function() {  
@@ -37,6 +35,16 @@ decrementButton.onclick = function() {
         testPlayer.decrementScore();
         scoreText.text(testPlayer.score);
     }
+}
+
+var addNewPlayerDiv = function() {
+    console.log("addNewPlayerDiv() triggered...");
+    $("#players").append('<input type="text" id="player-name" class="player-name-field" placeholder="Who\'s playing?  Click to enter your name!"/>'
+                + '<h2 id="score" class="headline-secondary--grouped" style = "margin-bottom: 10px"></h2>'
+                + '<div class = "buttons-group">'
+                + '<button id="decrement" class="btn--default">- 1</button>'
+                + '<button id="increment" class="btn--default">+ 1</button>'
+                + '</div>');
 }
 
 $("footer").mouseenter(function() {
@@ -60,3 +68,21 @@ function toggleMenu() {
 }
 //Lazy area where I should be making a new JS file but just want to test some things
 
+$("#test-button").click(function() {
+    addNewPlayerDiv();
+});
+
+$("#add-player-button").click(function() {
+    addNewPlayerDiv();
+});
+
+$("#player-name").focusout(function() {
+    $(this).css("color", "grey");
+    $(this).toggleClass("edit");
+    $(this).attr("placeholder", "");
+});
+
+$("#player-name").focusin(function() {
+    $(this).css("color", "black");
+    $(this).toggleClass("edit");
+});
