3aa11498b9809a43369c3930921af6c809825ee3
tzemati <tzemati@gmail.com>
2015-08-25 12:29:35 -0400

    changed way more than I should have

--- js/ui_controller.js
+++ js/ui_controller.js
@@ -3,20 +3,27 @@ var incrementButton = document.getElementById("increment");
 var decrementButton = document.getElementById("decrement");
 var progressText = document.getElementById("progress");
 var testPlayer = new Player("Jerry", 0);
+var players = [new Player("Player 1", 0), new Player("Player 2", 0)];
 var testGame = new Game();
 var numberOfPlayers = 2;
 var defaultPlayerNameText = "Who's playing? Click to enter your name.";
 
+var closePlayerDiv = '<div class="remove-player">X</div>'
+
 //Set initial score to 0
 //scoreText.innerHTML = 0;
 $(document).ready(function() {
-    scoreText.text(0);
+    
     //$("#player-name").css("width", "50%");
     $("#menu-toggle").hide();
     $("#number-players-selector").hide();
     $("#close-menu").hide();
+                 
+                 console.log(players[0]);
     
-    $("#players").append("<br /><button id='add-player-button'>Add</button>");
+    bindElements();
+    
+    //$("#players").append("<br /><button id='add-player-button'>Add</button>");
 });
 
 //$("#add-player-button").on("click", function() {  
@@ -39,9 +46,12 @@ $(document).ready(function() {
 //}
 
 var addNewPlayerDiv = function() {
-    ++numberOfPlayers;
+    
+//    ++numberOfPlayers;
+    players[numberOfPlayers] = new Player("Player " + ++numberOfPlayers, 0);
+    
     console.log("addNewPlayerDiv() triggered...");
-    $("#players").append('<input type="text" id="player-' + numberOfPlayers + '-name" class="player-name-field" placeholder="Who\'s playing?  Click to enter your name!"/>'
+    $("#players").append('<input type="text" id="player-' + numberOfPlayers + '-name" class="player-name-field" placeholder="Player ' + numberOfPlayers + '"/>'
                 + '<h2 id="score" class="headline-secondary--grouped" style = "margin-bottom: 10px"></h2>'
                 + '<div class = "buttons-group">'
                 +   '<button class="add-2-train btn--default">2</button>'
@@ -50,6 +60,7 @@ var addNewPlayerDiv = function() {
                 +   '<button class="add-5-train btn--default">5</button>'
                 +   '<button class="add-6-train btn--default">6</button>'
                 + '</div>');
+    console.log(players[2]);
 }
 
 $("footer").mouseenter(function() {
@@ -72,7 +83,7 @@ function toggleMenu() {
     $("#menu-toggle").toggle();
 }
 
-$("#add-2-train").click(function() {
+$(".add-2-train").click(function() {
     console.log("add-2-train clicked");
    testPlayer.incrementScore(2);
     updateScore();
@@ -90,7 +101,7 @@ $("#test-button").click(function() {
 });
 
 $("#add-player-div").click(function() {
-    alert(addNewPlayerDiv());
+    addNewPlayerDiv();
 });
 
 $(".player-name-field").focusout(function() {
@@ -108,6 +119,9 @@ $(".player-name-field").focusin(function() {
     $(this).append("<button>Done</button>");
 });
 
+var nameBoxFocusIn = function(element) {
+}
+
 $(".remove-player").click(function () {
 	console.log($(this) + " happened.");
     $(this).toggle();
@@ -116,3 +130,8 @@ $(".remove-player").click(function () {
 $("#player-3-name").focusin(function() {
     alert("Player 3 just got focus");
 });
+
+var bindElements = function() {
+   
+
+}
