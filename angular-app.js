fa2540d13d7b68f862cf3e2c598496d94b90cac9
AaronPope <tzemati@gmail.com>
2015-11-03 22:09:43 -0500

    added counter for tracking remaining trains per player; added really bad pixel art for train

--- angular-app.js
+++ angular-app.js
@@ -1,30 +1,85 @@
 var counterApp = angular.module("counterApp", []);
+
+        counterApp.service('sharedProperties', function () {
+            var numberOfPlayers = 0;
+
+            return {
+                getNumberOfPlayers: function () {
+                    return numberOfPlayers;
+                },
+                setNumberOfPlayers: function(number) {
+                    numberOfPlayers = number;
+                }
+            };
+        });
+
+        counterApp.controller('playersCtrl', function($scope) {
+            $scope.setNumberOfPlayers = function(numberOfPlayers) {
+                alert("New number of players is: " + sharedProperties.getNumberOfPlayers);
+                sharedProperties.setNumberOfPlayers(numberOfPlayers);
+                
+            }
+        });
              
         counterApp.controller('scoresCtrl', function($scope) {
-            $scope.twoTrainLengthScore = 2;
-            $scope.threeTrainLengthScore = 4;
-            $scope.fourTrainLengthScore = 7;
-            $scope.fiveTrainLengthScore = 10;
-            $scope.sixTrainLengthScore = 15;
+            var twoTrainLengthScore = 2;
+            var threeTrainLengthScore = 4;
+            var fourTrainLengthScore = 7;
+            var fiveTrainLengthScore = 10;
+            var sixTrainLengthScore = 15;
             
             $scope.playerScore = 0;
+            $scope.remainingTrains = 45;
             
             
-            $scope.addScore = function(value) {
-                $scope.playerScore += value;
+            $scope.addScore = function(routeLength) {
+                updateRemainingTrains(routeLength);
+                
+                switch(routeLength) {
+                    case 2:
+                        updateScore(twoTrainLengthScore);                        
+                        break;
+                        
+                    case 3:
+                        updateScore(threeTrainLengthScore);
+                        break;
+                        
+                    case 4:
+                        updateScore(fourTrainLengthScore);
+                        break;
+                    
+                    case 5:
+                        updateScore(fiveTrainLengthScore);
+                        break;
+                        
+                    case 6:
+                        updateScore(sixTrainLengthScore);
+                        break;
+                    
+                    default:
+                        break;
+                }
             };
+            
+            function updateScore(points) {
+                $scope.playerScore += points;
+            }
+            
+            function updateRemainingTrains(routeLength) {
+                $scope.remainingTrains -= routeLength;
+            }
         })
         .directive('testTemplate', function($http, $compile) {
                 return {
                     transclude: true,
-                    templateUrl: 'template.html' 
+                    templateUrl: 'ticket-to-ride-counter-template.html' 
                 }
             }
         )
         .directive('genericCounter', function($http, $compile) {
                 return {
                     transclude: true,
-                    templateUrl: 'generic-counter.html' 
+                    templateUrl: 'generic-counter-template.html' 
                 }
             }
         );
