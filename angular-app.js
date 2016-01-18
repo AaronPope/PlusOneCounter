c2fdf90a7696274a52f869745b82808d5debd7df
AaronPope <tzemati@gmail.com>
2016-01-17 22:20:01 -0500

    commented out old jQuery, from before switch to Angular

--- angular-app.js
+++ angular-app.js
@@ -21,27 +21,29 @@ counterApp.module('myApp', ['ngMaterial'])
     .accentPalette('orange');
 });
         
-        $("button").mousedown(function() {
-           toggleButtonBackgroundPositive($(this));
-        })
-        .mouseup(function() {
-           toggleButtonBackgroundPositive($(this));
-        })
-        //removes the background color even if the mouse is pulled away from the button
-        .mouseleave(function() {
-            if($(this).hasClass("click-background-color-positive")) {
-                toggleButtonBackgroundPositive($(this));
-            }
-        });
-        
-        var toggleButtonBackgroundPositive = function(element) {
-            $(element).toggleClass("click-background-color-positive");
-        }
-        
-        var toggleButtonBackgroundNegative = function(element) {
-            $(element).toggleClass("click-background-color-negative");
-        }
-        
-        $("#reset-scores-option").click(function() {
-            alert("it was clicked");
-        });
+
+    //*** Remnant from pre-Angular; leaving to remind me of concepts that may or may not be added again ***//
+//        $("button").mousedown(function() {
+//           toggleButtonBackgroundPositive($(this));
+//        })
+//        .mouseup(function() {
+//           toggleButtonBackgroundPositive($(this));
+//        })
+//        //removes the background color even if the mouse is pulled away from the button
+//        .mouseleave(function() {
+//            if($(this).hasClass("click-background-color-positive")) {
+//                toggleButtonBackgroundPositive($(this));
+//            }
+//        });
+//        
+//        var toggleButtonBackgroundPositive = function(element) {
+//            $(element).toggleClass("click-background-color-positive");
+//        }
+//        
+//        var toggleButtonBackgroundNegative = function(element) {
+//            $(element).toggleClass("click-background-color-negative");
+//        }
+//        
+//        $("#reset-scores-option").click(function() {
+//            alert("it was clicked");
+//        });
